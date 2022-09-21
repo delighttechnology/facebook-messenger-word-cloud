24b3560383a6f04051e3c90bb235bbf0e4ebe4e6
Delight Tech <delight.technology.office@gmail.com>
2022-09-21 22:59:48 +0200

    removed plot lib

--- src/fb_word_cloud.py
+++ src/fb_word_cloud.py
@@ -1,12 +1,11 @@
 import sys, os, pathlib, argparse       #pip install argparse
-import string, re
+import string, re                       #pip install regex
 import numpy as np                      #pip install numpy
 import pandas as pd                     #pip install pandas
-import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup           #pip install beautifulsoup4
 from unidecode import unidecode         #pip install Unidecode
-from wordcloud import WordCloud
-from PIL import Image
+from wordcloud import WordCloud         #pip install wordcloud
+from PIL import Image                   #pip install Pillow
 
 
 def dir_path(string):
@@ -35,12 +34,27 @@ def dataFrame_to_dict(df):
     df = df.set_index('Words').to_dict()['Count']
     return df
 
+def remove_punctation(st):
+    for c in string.punctuation:
+        st = st.replace('?',' ').replace('!',' ').replace(',',' ').replace('.',' ').replace('"',' ').replace('/',' ')#.replace(':',' ').replace('(',' ').replace(')',' ')
+        return st
+
+def lower_split(st):
+    st = st.lower()
+    return st
+
+def clean_message_string(st):
+    new = unidecode(st)
+    new = remove_punctation(new)
+    new = lower_split(new)
+    return new
+
+
 #List of arguments to provide
 parser = argparse.ArgumentParser()
 parser.add_argument('--path', '-p', help="Type a folder directory containing html files with messages", type=dir_path)
 parser.add_argument('--exclude', '-e', help="Type which words or letters to exclude separated with a comma", type= str)
 parser.add_argument('--image', '-i', help="Type png/jpg file path to create a mask", type=file_img)
-
 
 #Assign parameters to variables and check if --exclude exists
 args = parser.parse_args()
@@ -65,7 +79,7 @@ if(os.path.exists(path + "\combined.html")==False):
 
 print("Combining html files   -    DONE [1/6]")
 
-
+#Decomposing html file based on div classes
 with open(path + "\combined.html",encoding="utf8") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
 
@@ -97,23 +111,7 @@ messages = re.sub(r"\S*https?:\S*", "", messages)   #Remove links from string
 print("Clensing div's         -    DONE [3/6]")
 
 
-def remove_punctation(st):
-    for c in string.punctuation:
-        st = st.replace('?',' ').replace('!',' ').replace(',',' ').replace('.',' ').replace('"',' ').replace('/',' ')#.replace(':',' ').replace('(',' ').replace(')',' ')
-        return st
-
-def lower_split(st):
-    st = st.lower()
-    return st
-
-def clean_message_string(st):
-    new = unidecode(st)
-    new = remove_punctation(new)
-    new = lower_split(new)
-    return new
-
 # Creating an array to fill with the normalized text
-dump = []
 messagesCleansed = clean_message_string(messages)
 
 
@@ -121,6 +119,7 @@ print("Remove special chars   -    DONE [4/6]")
 
 
 # Fill an array with the list of cleansed words
+dump = []
 messagesCleansed= messagesCleansed.split()
 dump.extend(messagesCleansed)
 
@@ -138,15 +137,10 @@ print("Data Frame created     -    DONE [5/6]")
 if args.exclude is not None:
     df = df[df.Words.isin(parameters_list) == False]
 
-
-# Printing DataFrame values
-#pd.set_option('display.min_rows', 200)
-#pd.set_option('display.max_rows', 400)
-#print(df)
-
-# Save file
+#Save to CSV file
 df.to_csv(path + "\WordCount.csv",index=False)
 
+#Check if .png file contain transparent backgroud and change it to white
 if args.image is not None:
     if img_para.endswith('.png'):
         foregroud = Image.open(img_para).convert("RGBA")
@@ -160,7 +154,6 @@ else:
 
 #Generate Word Cloud
 wc = WordCloud(background_color="white", width=1400, height=1000, max_words=300,mask=mask_img,repeat=True,min_font_size=4).generate_from_frequencies(dataFrame_to_dict(df))
-
 
 # store to file
 wc.to_file(path + "\WordCloud.png")
